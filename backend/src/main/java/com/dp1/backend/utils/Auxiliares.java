e48d1bc2d9da78c9463b9cd3d7c8bd6dd73ce86c
Xnehil <harvy.martinezm@pucp.edu.pe>
2024-04-07 19:15:33 -0500

    Avance

--- backend/src/main/java/com/dp1/backend/utils/Auxiliares.java
+++ backend/src/main/java/com/dp1/backend/utils/Auxiliares.java
@@ -3,10 +3,16 @@ package com.dp1.backend.utils;
 import org.apache.commons.math3.distribution.NormalDistribution;
 import org.apache.commons.math3.special.Gamma;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.dp1.backend.models.Aeropuerto;
+import com.dp1.backend.models.Envio;
 import com.dp1.backend.models.Paquete;
+import com.dp1.backend.models.Vuelo;
 
 public class Auxiliares {
-    public static double[][] levy(int n, int m, double beta) {
+    public static double[]levy(int n,  double beta) {
         double num = Gamma.gamma(1 + beta) * Math.sin(Math.PI * beta / 2);
         double den = Gamma.gamma((1 + beta) / 2) * beta * Math.pow(2, (beta - 1) / 2);
         double sigma_u = Math.pow(num / den, 1 / beta);
@@ -14,18 +20,53 @@ public class Auxiliares {
         NormalDistribution distU = new NormalDistribution(0, sigma_u);
         NormalDistribution distV = new NormalDistribution(0, 1);
 
-        double[][] z = new double[n][m];
+        double[] z = new double[n];
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
                 double u = distU.sample();
                 double v = distV.sample();
-                z[i][j] = u / Math.pow(Math.abs(v), 1 / beta);
-            }
+                z[i] = u / Math.pow(Math.abs(v), 1 / beta);
         }
         return z;
     }
 
-    public static double fitness (Paquete paquete, int[] solucion){
+    public static double[] brown(int n) {
+        NormalDistribution dist = new NormalDistribution(0, 1);
+        double[] brown = new double[n];
+        for (int i = 0; i < n; i++) {
+            brown[i] = dist.sample();
+        }
+        return brown;
+    }
+
+    public static double fitnessTotal(int[] solucion, HashMap<String, Aeropuerto> aeropuertos,
+            HashMap<Integer, Vuelo> vuelos, HashMap<Integer, Envio> envios, ArrayList<Paquete> paquetes) {
+        // Aquí tengo la solución de todos los paquetes, cada n elementos es un paquete
+        int n = aeropuertos.size();
+        double fitnessTotal = 0;
+        double fitness;
+        for (int i = 0; i < solucion.length; i += n) {
+            // Enviar a fitnessUnPaquete la solución de un paquete. Es decir, los n elementos de la solución
+            int start = i;
+            int end = i + n;
+            fitness = fitnessUnPaquete(solucion, aeropuertos, vuelos, envios, paquetes, start, end);
+            fitnessTotal += fitness;
+        }
+        return fitnessTotal;
+    }
+
+    public static double fitnessUnPaquete(int[] solucion, HashMap<String, Aeropuerto> aeropuertos,
+            HashMap<Integer, Vuelo> vuelos, HashMap<Integer, Envio> envios, ArrayList<Paquete> paquetes, int start, int end) {
+        // Aquí tengo la solución de un solo paquete
+        double fitness=0;
+        for (int i = start; i < end; i++) {
+            //Añadir al fitness la distancia entre los aeropuertos de la solución
+            //Añadir bonificación si el paquete llega a tiempo
+            //Añadir penalización grave si el paquete no llega a tiempo
+            //Añadir bonificación si el paquete llega a su destino
+            //Añadir penalización grave si el paquete no llega a su destino
+
+            fitness+=1;
+        }
         return 0;
     }
 }
--- backend/src/main/java/com/dp1/backend/utils/MPA.java
+++ backend/src/main/java/com/dp1/backend/utils/MPA.java
@@ -2,6 +2,9 @@ package com.dp1.backend.utils;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
+
+import org.apache.commons.math3.distribution.NormalDistribution;
 
 import com.dp1.backend.models.Aeropuerto;
 import com.dp1.backend.models.Envio;
@@ -12,7 +15,24 @@ public class MPA {
     //Marine predator algorithm
     public static int[][] run(HashMap<String, Aeropuerto> aeropuertos, HashMap<Integer, Vuelo> vuelos, HashMap<Integer, Envio> envios,
                                 ArrayList<Paquete> paquetes, int maxIter, int popSize){
+        //La dimensión de la solución
+        int dim = paquetes.size()*aeropuertos.size();
         //Generar población inicial
+        int [][] Presa = new int[popSize][dim];
+        int [][] Elite = new int[popSize][dim];
+
+        for (int i = 0; i < popSize; i++) {
+            Presa[i] = inicializar(aeropuertos.size(), paquetes.size(), vuelos.size());
+        }
+
+        int masApta=solucionMasApta(Presa, aeropuertos, vuelos, envios, paquetes);
+
+        for (int i = 0; i < popSize; i++) {
+            Elite[i]=Presa[masApta];
+        }
+
+        double FADs =0.2;
+
 
         for (int i = 0; i < maxIter; i++) {
             //Evaluar población
@@ -20,14 +40,42 @@ public class MPA {
 
             //Actualizar memoria
 
-            if (i < maxIter/3) {
-                //Exploración
-            } else if (i < 2*maxIter/3) {
-                //Explotación
-            } else {
-                //Exploración y explotación
-            }
+            //Vector Levy
+            double[] levy= Auxiliares.levy(dim, 1.5); 
+            //Vector brown (normal distribution)
+            double[] brown = Auxiliares.brown(dim);
+            Random rand = new Random();
+            double P = 0.5;
+            double CF = Math.pow((1 - (double)i/maxIter), (2 * (double)i/maxIter));
 
+            double[][] stepsize = new double[popSize][dim];
+            for (int j = 0; j < popSize; j++) {
+                for (int k = 0; k < Presa[j].length; k++) {
+                    double R = rand.nextDouble();
+                    // Phase 1 (Eq.12)
+                    if (i < maxIter / 3) {
+                        stepsize[j][k] = brown[k] * (Elite[j][k] - Presa[j][k]*brown[k]);
+                        Presa[j][k] = (int)Math.round(Presa[j][k] + P*R*stepsize[j][k]);
+                    }
+                    // Phase 2 (Eqs. 13 & 14)
+                    else if (i > maxIter / 3 && i < 2 * maxIter / 3) {
+                        if (j < Presa.length / 2) {
+                            stepsize[j][k] = levy[k] * (Elite[j][k] - levy[k] * Presa[j][k]);
+                            Presa[j][k] = (int)Math.round(Presa[j][k] + P * R * stepsize[j][k]);
+                            
+                        } else {
+                            stepsize[j][k] = brown[k] * (brown[k] * Elite[j][k] - Presa[j][k]);
+                            Presa[j][k] = (int)Math.round(Elite[j][k] + P * CF * stepsize[j][k]);
+                        }
+                    }
+                    // Phase 3 (Eq. 15)
+                    else {
+                        stepsize[j][k] = levy[k] * (levy[k] * Elite[j][k] - Presa[j][k]);
+                        Presa[j][k] = (int)Math.round(Elite[j][k] + P * CF * stepsize[j][k]);
+                    }
+                }
+
+            }
             //Actualizar elite
 
             //Actualizar memoria
@@ -38,15 +86,33 @@ public class MPA {
         return new int[1][1];
     }
 
-    public static int[][] inicializar(int numAeropuertos, int paquetes){
-        int[][] poblacion = new int[paquetes][numAeropuertos];
-        for (int i = 0; i < paquetes; i++) {
-            for (int j = 0; j < numAeropuertos; j++) {
-                poblacion[i][j] = (int) (Math.random() * 2);
+    public static int[] inicializar(int numAeropuertos, int paquetes, int numVuelos){
+        int max=numVuelos;
+        int min=1;
+        int[] inicial= new int[paquetes*numAeropuertos];
+        for (int i = 0; i < inicial.length; i++) {
+            inicial[i]=(int)(Math.random()*(max-min+1)+min);
+        }
+        return inicial;
+    }
+
+    public static int solucionMasApta(int[][] poblacion, HashMap<String, Aeropuerto> aeropuertos, HashMap<Integer, Vuelo> vuelos, HashMap<Integer, Envio> envios,
+                                        ArrayList<Paquete> paquetes){
+        //Cada fila de población es una solución
+        //Cada solución tiene las rutas de todos los paquetes
+        //Cada n elementos de la solución corresponden a la ruta de un paquete
+        //Dicho n es igual al número de aeropuertos
+        int masApta=0;
+        double aptitudMax=0;
+        double aptitud;
+        for (int i = 0; i < poblacion.length; i++) {
+            aptitud=Auxiliares.fitnessTotal(poblacion[i], aeropuertos, vuelos, envios, paquetes);
+            if (aptitud>aptitudMax) {
+                aptitudMax=aptitud;
+                masApta=i;
             }
         }
-
-        return poblacion;
+        return masApta;
     }
 
 
