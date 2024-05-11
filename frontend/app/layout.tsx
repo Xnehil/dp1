cc206db445283d8021af8c63799dbbb987ce5109
Xnehil <harvy.martinezm@pucp.edu.pe>
2024-05-10 21:52:27 -0500

    Arreglo dependencias

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Montserrat } from "next/font/google";
-import NextBreadcrumb from '@/components/breadcrumb/NextBreadcrumb'
+import NextBreadcrumb from "@/components/breadcrumb/NextBreadcrumb";
 
 import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
@@ -23,18 +23,28 @@ export default function RootLayout({
                 <title>RedEx</title>
             </head>
 
-      <body className={`${inter.className} bg-[#EFEFEF] w-auto h-screen`}>
-        <Navbar />
-        <main className={`h-screen pt-4`}>
-          <div className="grid grid-cols-[1fr_38fr_1fr]">
-            <div></div>
-            <div>
-              {children}
-            </div>
-            <div></div>
-          </div>
-        </main>
-      </body>
-    </html>
-  );
+            <body className={`${inter.className} bg-[#EFEFEF] w-auto h-screen`}>
+                <Navbar />
+                <main className={`h-screen pt-4`}>
+                    <div className="grid grid-cols-[1fr_38fr_1fr]">
+                        <div></div>
+                        <div>
+                            {
+                                <NextBreadcrumb
+                                    homeElement={"Inicio"}
+                                    separator={<span>/</span>}
+                                    activeClasses="text-gray-700 font-bold text-sm"
+                                    containerClasses="flex py-5 bg-transparent rounded-t-md text-black"
+                                    listClasses="px-4 text-sm text-slate-700 hover:text-blue-700"
+                                    capitalizeLinks
+                                />
+                            }
+                            {children}
+                        </div>
+                        <div></div>
+                    </div>
+                </main>
+            </body>
+        </html>
+    );
 }
