cae7f284dec30bced2ada9b58ef2147f3ac78e1b
mauricio <arenales.mauricio24@gmail.com>
2024-05-29 14:46:32 -0500

    .

--- backend/src/main/java/com/dp1/backend/services/ACOService.java
+++ backend/src/main/java/com/dp1/backend/services/ACOService.java
@@ -20,31 +20,31 @@ import com.dp1.backend.utils.FuncionesLectura;
 public class ACOService {
     private static final Logger logger = LogManager.getLogger(ACOService.class);
     private ArrayList<Paquete> paquetes = new ArrayList<Paquete>();
-    
-    public boolean ejecutarAco(){
+
+    public boolean ejecutarAco() {
         paquetes.clear();
 
         HashMap<String, Aeropuerto> aeropuertos = new HashMap<String, Aeropuerto>();
         HashMap<Integer, Vuelo> vuelos = new HashMap<Integer, Vuelo>();
         HashMap<String, Envio> envios = new HashMap<String, Envio>();
         cargarDatos(aeropuertos, vuelos, envios, paquetes);
-        //Imprimir datos
+        // Imprimir datos
         logger.info("Ejecutando ACO para: ");
         logger.info("Aeropuertos: " + aeropuertos.size());
         logger.info("Vuelos: " + vuelos.size());
         logger.info("Envios: " + envios.size());
         logger.info("Paquetes: " + paquetes.size());
-        try{
-            //Medit tiempo de ejecución
+        try {
+            // Medit tiempo de ejecución
             Long startTime = System.currentTimeMillis();
             ACO.run_v2(aeropuertos, vuelos, envios, paquetes, 20);
             Long endTime = System.currentTimeMillis();
             Long totalTime = endTime - startTime;
             logger.info("Tiempo de ejecución: " + totalTime + " ms");
-            int paquetesEntregados=Auxiliares.verificacionTotalPaquetes(aeropuertos, vuelos, envios, paquetes);
+            int paquetesEntregados = Auxiliares.verificacionTotalPaquetes(aeropuertos, vuelos, envios, paquetes);
             logger.info("Paquetes entregados con función André: " + paquetesEntregados);
-        }
-        catch(Exception e){
+
+        } catch (Exception e) {
             logger.error("Error en ejecutarAco: " + e.getMessage());
             return false;
         }
@@ -52,31 +52,31 @@ public class ACOService {
 
     }
 
-    public boolean guardarRutas(){
-        try{
-            //To do fátima
-        }
-        catch(Exception e){
+    public boolean guardarRutas() {
+        try {
+            // To do fátima
+        } catch (Exception e) {
             logger.error("Error en guardarRutas: " + e.getMessage());
             return false;
         }
         return true;
     }
 
-    private void cargarDatos(HashMap<String, Aeropuerto> aeropuertos, HashMap<Integer, Vuelo> vuelos, HashMap<String, Envio> envios, ArrayList<Paquete> paquetes){
-        //Ahora mismo está leyendo datos de archivos, pero debería leer de la base de datos
+    private void cargarDatos(HashMap<String, Aeropuerto> aeropuertos, HashMap<Integer, Vuelo> vuelos,
+            HashMap<String, Envio> envios, ArrayList<Paquete> paquetes) {
+        // Ahora mismo está leyendo datos de archivos, pero debería leer de la base de
+        // datos
         aeropuertos.putAll(FuncionesLectura.leerAeropuertos("data/Aeropuerto.husos.v2.txt"));
         vuelos.putAll(FuncionesLectura.leerVuelos("data/planes_vuelo.v3.txt", aeropuertos));
         String rutaArchivos = "data/pack_enviado_";
-        String[] ciudades = { "SKBO", "SEQM", "SUAA", "SCEL", "SABE", "EBCI", "EHAM", "WMKK", "VIDP", "ZBAA"};
+        String[] ciudades = { "SKBO", "SEQM", "SUAA", "SCEL", "SABE", "EBCI", "EHAM", "WMKK", "VIDP", "ZBAA" };
         for (int i = 0; i < ciudades.length; i++) {
-            envios.putAll(FuncionesLectura.leerEnvios(rutaArchivos + ciudades[i] + ".txt", aeropuertos,20));
+            envios.putAll(FuncionesLectura.leerEnvios(rutaArchivos + ciudades[i] + ".txt", aeropuertos, 20));
         }
-    
+
         for (Envio e : envios.values()) {
             paquetes.addAll(e.getPaquetes());
         }
     }
-        
 
 }
--- frontend/app/vuelosEnVivo/page.tsx
+++ frontend/app/vuelosEnVivo/page.tsx
@@ -8,69 +8,69 @@ import { Vuelo } from "@/types/Vuelo";
 import { Aeropuerto } from "@/types/Aeropuerto";
 
 const Page = () => {
-    const bottomRef = useRef<HTMLDivElement>(null);
-    const apiURL = process.env.REACT_APP_API_URL_BASE;
-    const [vuelos, setVuelos] = useState<Vuelo[]>([]);
-    const [aeropuertos, setAeropuertos] = useState<Map<string, Aeropuerto>>(new Map());
-    const [cargado, setCargado] = useState(false);
+  const bottomRef = useRef<HTMLDivElement>(null);
+  const apiURL = process.env.REACT_APP_API_URL_BASE;
+  const [vuelos, setVuelos] = useState<Vuelo[]>([]);
+  const [aeropuertos, setAeropuertos] = useState<Map<string, Aeropuerto>>(new Map());
+  const [cargado, setCargado] = useState(false);
 
-    useEffect(() => {
-        bottomRef.current?.scrollIntoView({ behavior: "smooth" });
-    }, [cargado]);
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [cargado]);
 
-    useEffect(() => {
-        const fetchActiveFlights = () => {
-            axios.get(`${apiURL}/vuelo/enAire`)
-                .then((response) => {
-                    setVuelos(response.data);
-                    console.log("Vuelos cargados: ", response.data);
-                })
-                .catch((error) => {
-                    console.error(error);
-                });
-        };
-    
-        // Fetch active flights immediately
-        fetchActiveFlights();
-    
-        // Then fetch active flights every minute
-        const intervalId = setInterval(fetchActiveFlights, 60 * 1000);
-    
-        // Clean up the interval on component unmount
-        return () => clearInterval(intervalId);
-    }, []);
-    
-    useEffect(() => {
-        axios.get(`${apiURL}/aeropuerto`)
-            .then((response) => {
-                const aeropuertos = new Map<string, Aeropuerto>();
-                response.data.forEach((aeropuerto: Aeropuerto) => {
-                    aeropuertos.set(aeropuerto.codigoOACI, aeropuerto);
-                });
-                setAeropuertos(aeropuertos);
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-    }, []);
+  useEffect(() => {
+    const fetchActiveFlights = () => {
+      axios.get(`${apiURL}/vuelo/enAire`)
+        .then((response) => {
+          setVuelos(response.data);
+          console.log("Vuelos cargados: ", response.data);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    };
 
-    useEffect(() => {
-        if (vuelos && vuelos.length > 0 && aeropuertos.size > 0) {
-            setCargado(true);
-            // console.log("Aeropuertos cargados: ", aeropuertos);
-        }
-    }, [vuelos, aeropuertos]);
+    // Fetch active flights immediately
+    fetchActiveFlights();
 
-    return (
-        <>
-            {cargado && (
-                <div className="pb-4">
-                    <Mapa vuelos={vuelos.slice(0,50)} aeropuertos={aeropuertos} simulationInterval={10}/>
-                    <div ref={bottomRef}></div>
-                </div>
-            )}
-        </>
-    );
+    // Then fetch active flights every minute
+    const intervalId = setInterval(fetchActiveFlights, 60 * 1000);
+
+    // Clean up the interval on component unmount
+    return () => clearInterval(intervalId);
+  }, []);
+
+  useEffect(() => {
+    axios.get(`${apiURL}/aeropuerto`)
+      .then((response) => {
+        const aeropuertos = new Map<string, Aeropuerto>();
+        response.data.forEach((aeropuerto: Aeropuerto) => {
+          aeropuertos.set(aeropuerto.codigoOACI, aeropuerto);
+        });
+        setAeropuertos(aeropuertos);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (vuelos && vuelos.length > 0 && aeropuertos.size > 0) {
+      setCargado(true);
+      // console.log("Aeropuertos cargados: ", aeropuertos);
+    }
+  }, [vuelos, aeropuertos]);
+
+  return (
+    <>
+      {cargado && (
+        <div className="pb-4">
+          <Mapa vuelos={vuelos.slice(0, 50)} aeropuertos={aeropuertos} simulationInterval={10} />
+          <div ref={bottomRef}></div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Page;
