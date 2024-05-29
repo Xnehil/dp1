43a78cf37cf44d4d87e1d42909e09f0ea7e832e9
Xnehil <harvy.martinezm@pucp.edu.pe>
2024-05-28 23:02:32 -0500

    A ver

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -7,9 +7,9 @@ const nextConfig = {
     output: 'standalone',
     env: {
         // REACT_APP_API_URL_BASE: "http://localhost:8081",
-        REACT_APP_API_URL_BASE: "http://inf226-982-2b.inf.pucp.edu.pe:8081",
+        REACT_APP_API_URL_BASE: "http://inf226-982-2b.inf.pucp.edu.pe/api",
         // REACT_APP_WS_URL_BASE: "ws://localhost:8081",
-        REACT_APP_WS_URL_BASE: "ws://inf226-982-2b.inf.pucp.edu.pe:8081",
+        REACT_APP_WS_URL_BASE: "ws://inf226-982-2b.inf.pucp.edu.pe:80/api",
     },
 };
 
